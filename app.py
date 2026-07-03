52e2b483acf659753c6c3e4973ccd0c8231c509c
Rishabh Tandon <94946901+rtandon6342@users.noreply.github.com>
2026-07-03 12:00:13 +0530

    Fix greeting message in home route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return "HEllo THis is dummy web app for github actions testing"
+  return "HEllo THis is dummy web app for github actions testing !"
 
 if __name__ == "__main__":
     app.run()
